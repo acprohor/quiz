448bef4c8b1df2261235fc51e4d018efac93fb0a
acprohor <kasp300996>
2017-11-27 17:36:49 +0300

    all project test commit

--- app/src/main/java/com/example/acpro/quiz/MainActivity.java
+++ app/src/main/java/com/example/acpro/quiz/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.acpro.quiz;
 
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,10 +20,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    static boolean selectedState[] = new boolean[6]; // состояние вопроса. отвечен или нет.
+    static boolean rightAnswerState[] = new boolean[6]; // верно ли отвечен вопрос.
+    static int selectedButtonNum[] = new int[6]; // номер кнопки с правильным ответом
+    //static int imageNames[] = new int[6];
+    int mas[] = new int[6];
+    //static List<Integer> test = Arrays.asList(R.drawable.moon_shard, R.drawable.one, R.drawable.two, R.drawable.three);
+    static List<String> imageNames = Arrays.asList("one.jpg","two.jpg","three.jpg","four.jpg","clock_ability3.jpg","abaddona_ability4.jpg",
+            "chaos_ability1");
+
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -36,13 +57,13 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    static QuizCreator quizCreator1 = new QuizCreator();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -60,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        quizCreator1.createQuizArray();
+        quizCreator1.createNewSomething();
     }
 
 
@@ -106,16 +129,118 @@ public class MainActivity extends AppCompatActivity {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            //args.putli
             fragment.setArguments(args);
             return fragment;
         }
 
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            final TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            final TextView textView1 = (TextView) rootView.findViewById(R.id.textView);
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+            //String anotherRightAnswer = "nothing";
+            String anotherQuestion = "nothing";
+
+            ArrayList<String> anotherNewQuiz = new ArrayList<>();
+            anotherNewQuiz = quizCreator1.quizArray.get(getArguments().getInt(ARG_SECTION_NUMBER));
+            anotherQuestion = anotherNewQuiz.get(0);
+            final String anotherRightAnswer = anotherNewQuiz.get(1);
+            final String choose;
+            final List<Integer> answerButtons = Arrays.asList(R.id.button, R.id.button2, R.id.button3, R.id.button4);
+
+            final Button allButtons[] = new Button[4];
+
+            for (int i=0; i<4;i++){
+                allButtons[i] = (Button) rootView.findViewById(answerButtons.get(i));
+            }
+
+            // Set Choices.
+            for (int i = 0; i <4; i++){
+                allButtons[i].setText(anotherNewQuiz.get(i+2));
+            }
+
+            if (selectedState[getArguments().getInt(ARG_SECTION_NUMBER)]){
+                System.out.println("На этот вопрос уже был дан ответ.");
+                for (int i=0;i<4;i++){
+                    allButtons[i].setEnabled(false);
+                }
+
+                System.out.println("Выключили кнопки");
+                if (rightAnswerState[getArguments().getInt(ARG_SECTION_NUMBER)]){
+                    System.out.println("Ответ был дан верный, значит выделяем его зелёным.");
+                    allButtons[selectedButtonNum[getArguments().getInt(ARG_SECTION_NUMBER)]].getBackground().setColorFilter(Color.parseColor("#4CAF50"), PorterDuff.Mode.MULTIPLY);
+                }
+                else {
+                    System.out.println("Ответ был дан не верно, выделяем красным вариант, и правильный ответ");
+                    allButtons[selectedButtonNum[getArguments().getInt(ARG_SECTION_NUMBER)]].getBackground().setColorFilter(Color.parseColor("#F44336"), PorterDuff.Mode.MULTIPLY);
+                    System.out.println("Начинаем искать правильный ответ.");
+                    for (int j=0;j<4;j++){
+                        System.out.println("Текст из кнопки: " + allButtons[j].getText() + " сравним с правильным ответом: " + anotherRightAnswer);
+                        Button buttonTemp = allButtons[j];
+                        if (buttonTemp.getText().equals(anotherRightAnswer)){
+                            System.out.println("Нашли кнопку с правильным ответом, выделим её зелёным.");
+                            allButtons[j].getBackground().setColorFilter(Color.parseColor("#4CAF50"), PorterDuff.Mode.MULTIPLY);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            textView1.setText(anotherQuestion);
+            //imageView.setImageDrawable(Drawable.createFromPath("C:\\Users\\acpro\\AndroidStudioProjects\\quiz\\app\\src\\main\\res\\drawable\\moon_shard.jpg"));
+            //imageView.setImageDrawable(getResources().getDrawable(R.drawable.moon_shard));
+            //imageView.setImageDrawable(getResources().getDrawable(test.get(getArguments().getInt(ARG_SECTION_NUMBER))));
+            //imageView.setImageResource(R.drawable.);
+
+            AssetManager assets = getActivity().getAssets();
+            String answerStringOfPath = imageNames.get(getArguments().getInt(ARG_SECTION_NUMBER));
+            try {
+                imageView.setImageDrawable(Drawable.createFromStream(assets.open(answerStringOfPath), ""));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            for (int i = 0; i < allButtons.length; i++) {
+                final Button button = allButtons[i];
+                final int finalI = i;
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        selectedState[getArguments().getInt(ARG_SECTION_NUMBER)] = true;
+                        if (button.getText().equals(anotherRightAnswer)) {
+                            button.getBackground().setColorFilter(Color.parseColor("#4CAF50"), PorterDuff.Mode.MULTIPLY);
+                            textView1.setText("Nice, good job");
+                            for (Button button1:allButtons){
+                                button1.setEnabled(false);
+                            }
+                            rightAnswerState[getArguments().getInt(ARG_SECTION_NUMBER)] = true;
+                            selectedButtonNum[getArguments().getInt(ARG_SECTION_NUMBER)] = finalI;
+                        } else {
+                            button.getBackground().setColorFilter(Color.parseColor("#F44336"), PorterDuff.Mode.MULTIPLY);
+                            textView1.setText("No :( Right answer is " + anotherRightAnswer);
+                            for (Button button1:allButtons){
+                                button1.setEnabled(false);
+                            }
+                            for (int j =0; j < allButtons.length; j++){  // проверяем остальные кнопки и правильный ответ выделяем зелёным.
+                                if (allButtons[j].getText().equals(anotherRightAnswer)){
+                                    allButtons[j].getBackground().setColorFilter(Color.parseColor("#4CAF50"), PorterDuff.Mode.MULTIPLY);
+                                }
+                            }
+                            rightAnswerState[getArguments().getInt(ARG_SECTION_NUMBER)] = false;
+                            selectedButtonNum[getArguments().getInt(ARG_SECTION_NUMBER)] = finalI; //  переписать чтобы использовалось один раз вне оператора if
+                        }
+
+                    }
+                });
+            }
+
+            textView.setText(anotherRightAnswer);
             return rootView;
         }
     }
@@ -134,13 +259,15 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+
             return PlaceholderFragment.newInstance(position + 1);
+            //return PlaceholderFragment.newInstance(position + 1,list.get(position));
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 5 total pages.
+            return 5;
         }
     }
 }
